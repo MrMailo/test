5fcf72399c9e6bf62f05f96bf4daea84fa0b7355
MrMailo <60305860+MrMailo@users.noreply.github.com>
2020-03-07 04:57:34 +0200

    Add files via upload

---
+++ dreadlocks/js/main.js
@@ -0,0 +1,72 @@
+ 
+    $(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(500).fadeOut('slow');
+});
+
+ 
+
+$(document).ready(function(){
+    /**
+     * При прокрутке страницы, показываем или срываем кнопку
+     */
+    $(window).scroll(function () {
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 50) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+
+
+
+
+
+
+
+
+
+
+jQuery(function($) {
+	        $(window).scroll(function(){
+	            if($(this).scrollTop()>140){
+	                $('.header__menu').addClass('fixed');
+	            }
+	            else if ($(this).scrollTop()<140){
+	                $('.header__menu').removeClass('fixed');
+	            }
+	        });
+	    });
+
+
+
+
+
+
+$("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+$(".footer__menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+});
